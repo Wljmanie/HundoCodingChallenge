bc1f736e56ff00eec4d55bb3b94ad8845627feda
Wljmanie <45335409+Wljmanie@users.noreply.github.com>
2021-07-19 11:09:55 +0200

    Created the bare bone app
    Created the bare bone app, functionality.

--- js/Site.js
+++ js/Site.js
@@ -2,3 +2,60 @@
 function HelloWorld(){
     alert("Hello World!");
 }
+
+function test(){
+    alert("Test!");
+}
+
+function getValues(){
+    let startValue = Number.parseInt(document.getElementById("startValue").value);
+    //let endValue = Number.parseInt(document.getElementById("endValue").value);
+    
+    let endValue = document.getElementById("endValue").value;
+    
+
+    if(Number.isNaN(endValue)){
+        alert("NAN ALERT");
+    }
+    endValue = Number.parseInt(endValue);
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+        if(startValue == endValue){
+            alert("Please enter two different numbers.")
+        }
+        else{
+            createNumbers(startValue, endValue);
+        }
+    }
+    else{
+        alert("Please only enter intergers.");
+    } 
+}
+
+function createNumbers(sV, eV){
+    let numbers = [];
+    if(sV < eV){
+        for(let i = sV; i <= eV; i++){
+            numbers.push(i);
+        }
+    }
+    else{
+        for(let i = sV; i >= eV; i-- ){
+            numbers.push(i);
+        }
+    }
+    displayNumbers(numbers);  
+}
+
+function displayNumbers(numbers){
+    let tempRows = "";
+
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i]%2 == 0){
+            tempRows += `<tr><td class="even">${numbers[i]}</td></tr>\n`
+        }
+        else{
+            tempRows += `<tr><td class="odd">${numbers[i]}</td></tr>\n`   
+        }
+    }
+    document.getElementById("results").innerHTML = tempRows;
+}
